6cee93e852b7a4cb5f4152264464589cdebd9428
David Albear <davidalbearguevara@gmail.com>
2021-08-06 13:00:01 -0500

    Commit v2

--- js/dom/navbar.js
+++
@@ -1,8 +0,0 @@
-export default function (btnNav, listLinks) {
-  const $btn = document.getElementById(btnNav);
-  const $links = document.querySelector(listLinks);
-
-  $btn.addEventListener("click", (e) => {
-    $links.classList.toggle("nav--visible");
-  });
-}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-import navbar from "./dom/navbar.js";
+// import navbar from "./dom/navbar.js";
+// const d = document;
+
+// d.addEventListener('click' , e => {
+//     navbar("nav__toggle",".main-nav")
+// })
+
+
 const d = document;
+const navBtn = d.getElementById('btnNavOpen');
+const navMenu =  d.querySelector('.main-nav');
 
-d.addEventListener('click' , e => {
-    navbar("nav__toggle",".main-nav")
+
+navBtn.addEventListener('click',e =>{
+  navMenu.classList.toggle('is-active');
 })
-
